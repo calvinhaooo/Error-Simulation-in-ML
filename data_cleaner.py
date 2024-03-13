45a14f97986aa400a67872c327ff6a9f6b99cb97
Ifon <1565975426@qq.com>
2024-03-13 16:25:30 +0100

    add data clean methods and reformat data preprocessing

---
+++ data_cleaner.py
@@ -0,0 +1,38 @@
+from sklearn.decomposition import TruncatedSVD
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import LabelEncoder
+
+
+def remove_duplication(df, method='simple'):
+    new_df = df.copy()
+    if method == 'simple':
+        new_df = new_df.drop_duplicates()
+    elif method == 'similarity':
+        pass
+        # todo
+    return new_df
+
+
+def cluster_labels(df, label):
+    features = df['text']
+    labels = df[label]
+
+    # Encoding labels if they are categorical
+    label_encoder = LabelEncoder()
+    encoded_labels = label_encoder.fit_transform(labels)
+    text_vectorizer = TfidfVectorizer()
+    encoded_text = text_vectorizer.fit_transform(features)
+
+    # use TruncatedSVD to extract main features
+    n_components = 50
+    svd = TruncatedSVD(n_components=n_components, random_state=42)
+    reduced_text = svd.fit_transform(encoded_text)
+
+    # Creating k-NN classifier without specifying the number of neighbors
+    knn = KNeighborsClassifier()
+    knn.fit(reduced_text, encoded_labels)
+    new_labels = knn.predict(reduced_text)
+    # print(cluster_labels)
+    print(f"Number of label types after clustering is {len(set(new_labels))}.")
+    return new_labels
--- data_preprocessor.py
+++ data_preprocessor.py
@@ -1,23 +1,34 @@
+from pandas import DataFrame
 from pandas.core.dtypes.common import is_numeric_dtype, is_string_dtype
 
 
-def select_features(label, data, alpha=0.1):
-    categorical_columns = []
-    numerical_columns = []
-    text_columns = []
-    for col in data.columns:
-        data_type = data[col].dtype
-        null_num = data[col].isnull().sum()
-        feature_num = data[col].nunique()
-        if null_num / len(data) > alpha or col == label:
-            print(col, null_num / len(data))
+def select_features(label, data, alpha=0.1, beta=1e-3):
+    categorical_columns, numerical_columns, text_columns = [], [], []
+    for column in data.columns:
+        # skip label column
+        if column == label:
             continue
 
+        null_num = data[column].isnull().sum()
+        missing_percent = null_num / len(data)
+        if missing_percent > alpha:
+            missing_percentage = "{0:.2f}".format(missing_percent * 100)
+            print(f"Drop column {column} for {missing_percentage}% missing values")
+            continue
+
+        data_type = data[column].dtype
+        feature_num = data[column].nunique()
         if is_numeric_dtype(data_type):
-            numerical_columns.append(col)
-        elif feature_num <= len(data) * 0.001:
-            categorical_columns.append(col)
+            numerical_columns.append(column)
+        elif feature_num <= len(data) * beta:
+            categorical_columns.append(column)
         elif is_string_dtype(data_type):
-            text_columns.append(col)
+            text_columns.append(column)
 
     return categorical_columns, numerical_columns, text_columns
+
+
+def merge_text(data: DataFrame, text_columns, column_name):
+    data[column_name] = ''
+    for text_column in text_columns:
+        data[column_name] += ' ' + data[text_column]
